546d7002a04541a1601d57121c72e6ed7663e9d4
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-05 21:30:10 -0700

    socials

---
+++ src/components/socials/socials.component.tsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+
+import './socials.styles.css'
+
+const Socials = () => {
+
+    return (
+        <div className="my-socials">
+            <a className="my-link" href='https://www.linkedin.com/in/oscargutierrezaltamirano'>
+                <FontAwesomeIcon className="my-icon" icon={faLinkedin}/>
+            </a>
+            <a className="my-link" href='https://www.github.com/oscarg617'>
+                <FontAwesomeIcon className="my-icon" icon={faGithub}/>
+            </a>
+            <a className="my-link" href='mailto:oscargutierrez617@berkeley.edu'>
+                <FontAwesomeIcon className="my-icon" icon={faEnvelope}/>
+            </a>
+        </div>
+    )
+}
+
+export default Socials
