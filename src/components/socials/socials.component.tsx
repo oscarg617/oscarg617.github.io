0dc3477e89400fee60144294078040c342d0b137
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-05 22:38:44 -0700

    fixed hyperlink

--- src/components/socials/socials.component.tsx
+++ src/components/socials/socials.component.tsx
@@ -9,13 +9,13 @@ const Socials = () => {
 
     return (
         <div className="my-socials">
-            <a className="my-link" href='https://www.linkedin.com/in/oscargutierrezaltamirano'>
+            <a className="my-link" target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/oscargutierrezaltamirano'>
                 <FontAwesomeIcon className="my-icon" icon={faLinkedin}/>
             </a>
-            <a className="my-link" href='https://www.github.com/oscarg617'>
+            <a className="my-link" target="_blank" rel="noopener noreferrer" href='https://www.github.com/oscarg617'>
                 <FontAwesomeIcon className="my-icon" icon={faGithub}/>
             </a>
-            <a className="my-link" href='mailto:oscargutierrez617@berkeley.edu'>
+            <a className="my-link" target="_blank" rel="noopener noreferrer" href='mailto:oscargutierrez617@berkeley.edu'>
                 <FontAwesomeIcon className="my-icon" icon={faEnvelope}/>
             </a>
         </div>
