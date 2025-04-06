7bcdaf095d9039807bb7602e110e16239649624e
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-05 21:32:07 -0700

    basic layout with introduction, experience, and education

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
+import Education from './components/education/education.component';
+import Experience from './components/experience/experience.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Footer />
+        <Experience />
+        <Education />
     </div>
   );
 }
