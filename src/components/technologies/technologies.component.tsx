21a5679b24c10871e2a11fb5b0ecc39f5b686943
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-07 14:08:40 -0700

    added my tech stack

---
+++ src/components/technologies/technologies.component.tsx
@@ -0,0 +1,50 @@
+import python from '../../assets/python.svg'
+import java from '../../assets/java.svg'
+import c from '../../assets/c.svg'
+import flask from '../../assets/flask.svg'
+import pandas from '../../assets/pandas.svg'
+import postgresql from '../../assets/postgresql.svg'
+import git from '../../assets/git.svg'
+import react from '../../assets/react.svg'
+
+import './technologies.styles.scss'
+import { Link } from '@material-ui/core'
+
+const technologies = [
+    { "site": "https://www.python.org", "source": python, "title": "Python" },
+    { "site": "https://www.java.com", "source": java, "title": "Java" },
+    { "site": "https://www.gnu.org/software/gnu-c-manual", "source": c, "title": "C" },
+    { "site": "https://flask.palletsprojects.com/en/stable", "source": flask, "title": "Flask" },
+    { "site": "https://pandas.pydata.org", "source": pandas, "title": "Pandas" },
+    { "site": "https://www.postgresql.org", "source": postgresql, "title": "PostgreSQL" },
+    { "site": "https://git-scm.com", "source": git, "title": "Git" },
+    { "site": "https://react.dev/", "source": react, "title": "React" },
+]
+
+const Technologies = () => {
+    return (
+        <div className='technologies'>
+            <p>Tech Stack</p>
+            <div className='tech-list'>
+                {technologies.map((tech) => (
+                    <div className='tech-container' >
+                        <a href={tech.site} target='_blank' rel=' noopener noreferrer'>
+                            <img className='my-tech-item' src={tech.source} alt='' />
+                        </a>
+                        <h5 className='tech-title' >{tech.title}</h5>
+                    </div>
+                ))}
+            </div>
+{/*             
+            <img className='my-tech-item' src={java} width="auto" height={75} alt='' />
+            <img className='my-tech-item' src={c} width="auto" height={75} alt='' />
+            <img className='my-tech-item' src={flask} width="auto" height={75} alt='' />
+            <img className='my-tech-item' src={pandas} width="auto" height={75} alt='' />
+            <img className='my-tech-item' src={postgresql} width="auto" height={75} alt='' />
+            <img className='my-tech-item' src={git} width="auto" height={75} alt='' />
+            <img className='my-tech-item' src={react} width="auto" height={75} alt='' /> */}
+        </div>
+    )
+}
+
+export default Technologies
