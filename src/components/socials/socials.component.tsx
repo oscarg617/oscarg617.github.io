8fa8d95411c6720503c820f0dd0103d7897c8489
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-07 14:08:24 -0700

    added titles to socials links

--- src/components/socials/socials.component.tsx
+++ src/components/socials/socials.component.tsx
@@ -2,6 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faPython } from "@fortawesome/free-brands-svg-icons"
+import { faJava } from "@fortawesome/free-brands-svg-icons"
+import { faC } from "@fortawesome/free-solid-svg-icons"
+import { faFlask } from "@fortawesome/free-solid-svg-icons"
+import { faReact } from "@fortawesome/free-brands-svg-icons"
+
 
 import './socials.styles.scss'
 
@@ -10,13 +16,13 @@ const Socials = () => {
     return (
         <div className="my-socials">
             <a className="my-link" target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/oscargutierrezaltamirano'>
-                <FontAwesomeIcon className="my-icon" icon={faLinkedin}/>
+                <FontAwesomeIcon title="LinkedIn" className="my-icon" icon={faLinkedin}/>
             </a>
             <a className="my-link" target="_blank" rel="noopener noreferrer" href='https://www.github.com/oscarg617'>
-                <FontAwesomeIcon className="my-icon" icon={faGithub}/>
+                <FontAwesomeIcon title="GitHub" className="my-icon" icon={faGithub}/>
             </a>
             <a className="my-link" target="_blank" rel="noopener noreferrer" href='mailto:oscargutierrez617@berkeley.edu'>
-                <FontAwesomeIcon className="my-icon" icon={faEnvelope}/>
+                <FontAwesomeIcon title="Email" className="my-icon" icon={faEnvelope}/>
             </a>
         </div>
     )
