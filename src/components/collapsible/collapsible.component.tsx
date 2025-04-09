b2dbc399c39f288145aa3f16462e9288e0b3a94e
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-08 21:54:42 -0700

    fixed css for collapsible

--- src/components/collapsible/collapsible.component.tsx
+++ src/components/collapsible/collapsible.component.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { CButton, CCollapse, CCardBody } from '@coreui/react'
+import { CButton, CCollapse } from '@coreui/react'
 import '@coreui/coreui/dist/css/coreui.min.css';
 import './collapsible.styles.scss'
 
@@ -24,13 +24,10 @@ export const Collapsible: React.FC<CollapsibleProps> = ({ children, label }) => 
         style={{marginLeft:"0", marginBottom: "10px", backgroundColor: "#eeeeee", color: "black"}}
         className='projects-button'
       >
-        
         {label}
       </CButton>
       <CCollapse visible={visible}>
-          <CCardBody>
             {children}
-          </CCardBody>
       </CCollapse>
     </div>
   )
