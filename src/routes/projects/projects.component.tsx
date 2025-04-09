9e699ace3653158270f851f314fe0348cf4284c7
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-08 22:05:52 -0700

    added projects page

--- src/routes/projects/projects.component.tsx
+++ src/routes/projects/projects.component.tsx
@@ -1,9 +1,18 @@
+import ProjectCard from '../../components/project-card/project-card.component';
+import projects from '../../data/projects.json'
 import './projects.styles.scss'
 
 const Projects = () => {
+
     return (
         <div className='projects'>
-            <h1>Projects</h1>
+            <h1 className='projects-title' >Projects</h1>
+            <p className='projects-subtitle' >This is where I share the challenges and insights behind each project in my developer journey.</p>
+            <div className='projects-container'>
+                {projects.map((project) => (
+                    <ProjectCard key={project.title} {...project}/>
+                ))}
+            </div>
         </div>
     )
 }
