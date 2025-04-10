f53ab5586d0bbae0068eb37cc66968a0a0918626
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-09 20:23:44 -0700

    added image to project card component

--- src/components/project-card/project-card.component.tsx
+++ src/components/project-card/project-card.component.tsx
@@ -8,30 +8,35 @@ type projectCardProps  = {
     description: string,
     technologies: string[],
     links: any[],
+    image: string,
     [x:string]: any;
 }
 
-
 const ProjectCard : React.FC<projectCardProps> = (props) => {
+
     return (
         <div className='project-card-container'>
-            <Link to={`/projects/${props.title.toLowerCase()}`} className='projects-button'>
+            <Link to={`${props.title.toLowerCase()}`} className='project-button'>
             <CCollapse visible={true} >
-                <CCard className='projects-card' >
+                <CCard className='project-card' >
                     <CCardBody>
-                        <div className='project-card-heading'>
-                            <h2 className='project-card-heading-item'>{props.title}</h2>
-                        </div>
-                        <p>{props.description}</p>
-                        <div className='project-card-footer'>
-                            <ul className='project-card-tech-list' >
-                                {props.technologies.map((technology) => (
-                                    <li className='project-card-tech' >{technology}</li>
-                                ))}
-                                {props.links.map((link) => (
-                                    <a className={`project-card-link ${link.linkType.toLowerCase().split(' ').join('-') }`} target="_blank" rel="noopener noreferrer" href={link.link}>{link.linkType}</a>
-                                ))}
-                            </ul>
+                        <div className='project-card-content'>
+                            <div className='project-card-desc'>
+                                <div className='project-card-heading'>
+                                    <h2 className='project-card-heading-item'>{props.title}</h2>
+                                </div>
+                                <p className='project-card-subtitle'>{props.description}</p>
+                                <div className='project-card-footer'>
+                                    <ul className='project-card-tech-list' >
+                                        {props.technologies.map((technology) => (
+                                            <li className='project-card-tech' >{technology}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className='project-card-image-container'>
+                                <img  className='project-card-image' src={props.image} alt='' />
+                            </div>
                         </div>
                     </CCardBody>
                 </CCard>
