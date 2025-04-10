f2e6c177edfa9490f0d61fb9594578d6768088ab
Oscar Gutierrez Altamirano <oscargutierrez617@berkeley.edu>
2025-04-09 20:24:57 -0700

    added container to social link to ensure clicks on links are accurate

--- src/components/socials/socials.component.tsx
+++ src/components/socials/socials.component.tsx
@@ -3,23 +3,27 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
-
 import './socials.styles.scss'
 
 const Socials = () => {
 
-
     return (
         <div className="my-socials">
-            <a className="my-link" target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/oscargutierrezaltamirano'>
-                <FontAwesomeIcon title="" className="my-icon" icon={faLinkedin}/>
-            </a>
-            <a className="my-link" target="_blank" rel="noopener noreferrer" href='https://www.github.com/oscarg617'>
-                <FontAwesomeIcon title="GitHub" className="my-icon" icon={faGithub}/>
-            </a>
-            <a className="my-link" target="_blank" rel="noopener noreferrer" href='mailto:oscargutierrez617@berkeley.edu'>
-                <FontAwesomeIcon title="Email" className="my-icon" icon={faEnvelope}/>
-            </a>
+            <div className="my-link-container">
+                <a className="my-link" target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/oscargutierrezaltamirano'>
+                    <FontAwesomeIcon title="" className="my-icon" icon={faLinkedin}/>
+                </a>
+            </div>
+            <div className="my-link-container">
+                <a className="my-link" target="_blank" rel="noopener noreferrer" href='https://www.github.com/oscarg617'>
+                    <FontAwesomeIcon title="GitHub" className="my-icon" icon={faGithub}/>
+                </a>
+            </div>
+            <div className="my-link-container">
+                <a className="my-link" target="_blank" rel="noopener noreferrer" href='mailto:oscargutierrez617@berkeley.edu'>
+                    <FontAwesomeIcon title="Email" className="my-icon" icon={faEnvelope}/>
+                </a>
+            </div>
         </div>
     )
 }
